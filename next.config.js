8b01ef6feaf35abd85587380004be41f551723aa
Ner Karso <nerpkarso@gmail.com>
2024-01-12 19:14:34 -0300

    chore: move away from monorepo

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        hostname: '*',
-      },
-    ],
+    remotePatterns: [{ hostname: '*' }],
     unoptimized: true,
   },
   experimental: {
